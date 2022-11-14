6c2ea7ad95bed66ba708078cdf536840b1bc1bd1
praveen-iyer <apraveen1999@gmail.com>
2022-11-14 10:30:46 -0800

    improved code readability tripadvisor spider

--- data_scraper/data_scraper/spiders/tripadvisor_attraction_spider.py
+++ data_scraper/data_scraper/spiders/tripadvisor_attraction_spider.py
@@ -30,54 +30,46 @@ class AttractionSpider(scrapy.Spider):
             data["attraction_n_reviews"] = None
             return data
 
-
-        if response.css('div.vAiJm h1.biGQs._P.fiohW.eIegw::text').get() is not None:
-            attraction_name = response.css('div.vAiJm h1.biGQs._P.fiohW.eIegw::text').get().strip()
-
-            if not response.css('div.ZhNYD div.MJ').getall():
-                attraction_data = give_empty_data()
-
-            else:
-
-                location = response.css('div.ZhNYD div.MJ button.UikNM.G.B-._S._T.c.G.P0.wSSLS.wnNQG.raEkE span.biGQs._P.XWJSj.Wb::text').get()
-                if not location:
-                    location = response.css('div.ZhNYD div.MJ span.biGQs._P.XWJSj.Wb::text').getall()
-                    if location:
-                        location = location[-1]
-                    else:
-                        location = ""
-                # location = "2800 E. Observatory Rd., Los Angeles, CA 90027-1299"
-
-                if location:
-                    delimeter = "CA "
-                    zip_code_len = 5
-                    zip_code = []
-                    for i in range(location.find(delimeter)+len(delimeter),len(location)):
-                        if location[i].isdigit():
-                            zip_code.append(location[i])
-                            if len(zip_code)==zip_code_len:
-                                break
-                    zip_code = "".join(zip_code)
-
-                    rev_rat = response.css('div.eSDnY div.bdeBj.e div.yFKLG div.f.u.j')
-                    if rev_rat.getall():
-                        rating = float(rev_rat.css('div.biGQs._P.fiohW.hzzSG.uuBRH::text').get().strip())
-                        n_reviews = rev_rat.css('span.biGQs._P.pZUbB.biKBZ.KxBGd::text').get()
-                        n_reviews = int("".join((c for c in n_reviews if c.isdigit())))
-                    else:
-                        rating = None
-                        n_reviews = None
-
-                    attraction_data["attraction_name"] = attraction_name
-                    attraction_data["attraction_address"] = location
-                    attraction_data["zip_code"] = zip_code
-                    attraction_data["attraction_rating"] = rating
-                    attraction_data["attraction_n_reviews"] = n_reviews
-                else:
-                    attraction_data = give_empty_data()
-
-            yield attraction_data
-        
+        attraction_name = response.css('div.vAiJm h1.biGQs._P.fiohW.eIegw::text').get()
+        if attraction_name is None:
+            return give_empty_data()
         else:
-            attraction_data = give_empty_data()
-            yield attraction_data
+            attraction_name = attraction_name.strip()
+
+        if len(response.css('div.ZhNYD div.MJ').getall())==0:
+            return give_empty_data()
+
+        location = response.css('div.ZhNYD div.MJ button.UikNM.G.B-._S._T.c.G.P0.wSSLS.wnNQG.raEkE span.biGQs._P.XWJSj.Wb::text').get()
+        if location is None:
+            location = response.css('div.ZhNYD div.MJ span.biGQs._P.XWJSj.Wb::text').getall()
+            if len(location)>0:
+                location = location[-1]
+            else:
+                return give_empty_data()
+        # location = "2800 E. Observatory Rd., Los Angeles, CA 90027-1299"
+
+        delimeter = "CA "
+        zip_code_len = 5
+        zip_code = []
+        for i in range(location.find(delimeter)+len(delimeter),len(location)):
+            if location[i].isdigit():
+                zip_code.append(location[i])
+                if len(zip_code)==zip_code_len:
+                    break
+        zip_code = "".join(zip_code)
+
+        rev_rat = response.css('div.eSDnY div.bdeBj.e div.yFKLG div.f.u.j')
+        if len(rev_rat.getall())>0:
+            rating = float(rev_rat.css('div.biGQs._P.fiohW.hzzSG.uuBRH::text').get().strip())
+            n_reviews = rev_rat.css('span.biGQs._P.pZUbB.biKBZ.KxBGd::text').get()
+            n_reviews = int("".join((c for c in n_reviews if c.isdigit())))
+        else:
+            return give_empty_data()
+
+        attraction_data["attraction_name"] = attraction_name
+        attraction_data["attraction_address"] = location
+        attraction_data["zip_code"] = zip_code
+        attraction_data["attraction_rating"] = rating
+        attraction_data["attraction_n_reviews"] = n_reviews
+
+        yield attraction_data
